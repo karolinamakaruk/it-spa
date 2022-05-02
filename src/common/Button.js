01eb58e9c6d832d41ffa0f48106c76208703deb9
karolinamakaruk <karolinamakaruk@gmail.com>
2022-05-03 00:11:55 +0200

    add id to button

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,10 +1,17 @@
 export function Button(options) {
-  const { text = "", classes = "", btnType ='button', btnForm ='', onClick = () => {} } = options;
+  const {
+    btnId = "",
+    text = "",
+    classes = "",
+    btnType = "button",
+    btnForm = "",
+    onClick = () => {},
+  } = options;
 
   const button = document.createElement("button");
-
+  button.setAttribute("id", btnId);
   button.setAttribute("type", btnType);
-  button.setAttribute('form', btnForm)
+  button.setAttribute("form", btnForm);
   button.innerText = text;
   button.className = classes;
   button.addEventListener("click", onClick);
