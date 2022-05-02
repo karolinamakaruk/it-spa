73a9c6fe793001783270ce96708708e745a08fbe
karolinamakaruk <karolinamakaruk@gmail.com>
2022-05-03 00:12:51 +0200

    add id to submitbutton

--- src/login/SubmitButtons.js
+++ src/login/SubmitButtons.js
@@ -6,21 +6,21 @@ export function SigninButton() {
     text: "Sign in",
     classes: "btn btn-secondary",
     onClick: async () => {
-        user = await loginCheck();
-        console.log("test2");
-     
+      user = await loginCheck();
+      console.log("test2");
     },
   });
 }
 
 export function SignupButton() {
   return Button({
+    btnId: "btnSignUp",
     text: "Sign up",
     classes: "btn btn-secondary",
-    btnType: "submit",
+    //btnType: "submit",
     btnForm: "tabs",
     onClick: () => {
-      console.log("sign up");
+      console.log("tito");
     },
     // async () => {
     //       user = await loginCheck();
